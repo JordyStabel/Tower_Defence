3030e07c637a3a7f10a4c0674862ae2795d6c444
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-03-25 13:01:06 +0200

    Added Mainmeny screen

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startingLevel = "MainLevel";
+
+	public void Play()
+    {
+        SceneManager.LoadScene(startingLevel);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting the game...");
+        Application.Quit();
+    }
+}
