14fd9239270d6a038bdc93d5f3d9263c1043a33f
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-04-08 16:19:52 +0200

    progress path generator v2

--- Assets/Scripts/RoadMarker.cs
+++ Assets/Scripts/RoadMarker.cs
@@ -12,57 +12,59 @@ public class RoadMarker : MonoBehaviour {
     private int random;
     private bool created = false;
 
-    private int maxAmount = 10;
-    private int currentAmount = 0;
-
     private void Start()
     {
         //Get the roadtemplate object form the hierarchy only during the start of this object
         roadTemplates = GameObject.FindGameObjectWithTag("Roads").GetComponent<RoadTemplates>();
 
         //Call the CreateRoad methode with 0.2 seconds delay
-        Invoke("CreateRoad", 0.2f);
+        Invoke("CreateRoad", 1f);
     }
 
     void CreateRoad()
     {
-        if (!created && currentAmount <= maxAmount)
+        if (created == false)
         {
             if (openingDirection == 1)
             {
                 //Need to spawn road with BOTTOM opening
                 random = Random.Range(0, roadTemplates.bottomRoads.Length);
-                Instantiate(roadTemplates.bottomRoads[random], transform.position, Quaternion.identity);
+                Instantiate(roadTemplates.bottomRoads[random], transform.position, roadTemplates.bottomRoads[random].transform.rotation);
             }
             else if (openingDirection == 2)
             {
                 //Need to spawn road with TOP opening
                 random = Random.Range(0, roadTemplates.topRoads.Length);
-                Instantiate(roadTemplates.topRoads[random], transform.position, Quaternion.identity);
+                Instantiate(roadTemplates.topRoads[random], transform.position, roadTemplates.topRoads[random].transform.rotation);
             }
             else if (openingDirection == 3)
             {
                 //Need to spawn road with LEFT opening
                 random = Random.Range(0, roadTemplates.leftRoads.Length);
-                Instantiate(roadTemplates.leftRoads[random], transform.position, Quaternion.identity);
+                Instantiate(roadTemplates.leftRoads[random], transform.position, roadTemplates.leftRoads[random].transform.rotation);
             }
             else if (openingDirection == 4)
             {
                 //Need to spawn road with RIGHT opening
                 random = Random.Range(0, roadTemplates.rightRoads.Length);
-                Instantiate(roadTemplates.rightRoads[random], transform.position, Quaternion.identity);
+                Instantiate(roadTemplates.rightRoads[random], transform.position, roadTemplates.rightRoads[random].transform.rotation);
             }
-            currentAmount++;
             created = true;
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter(Collider collision)
     {
-        //If a road collides with another road it will get destroyed
-        if (collision.CompareTag("RoadMarkerTag") && collision.GetComponent<RoadMarker>().created == true)
+        //If a road collides with another road this roadmarker will get destroyed, thus stop making other roads
+        if (collision.CompareTag("RoadMarkerTag"))
         {
             Destroy(gameObject);
+        }
+        else if (collision.CompareTag("Center"))
+        {
+            Destroy(gameObject);
+            Debug.Log("End the path here");
+            //End Road prefab needs to be spawn here
         }
     }
 }
